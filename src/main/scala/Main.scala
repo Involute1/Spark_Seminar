2a9de0a082ec3bc7e7f2af01e62597e0d3817c98
Luis.Nothvogel <L_noth@yahoo.de>
2023-04-10 12:25:15 +0200

    added simple example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.sql.SparkSession
 
+import java.util.Properties
+
 object Main {
   def main(args: Array[String]): Unit = {
 
@@ -8,24 +10,49 @@ object Main {
       .config("spark.master", "local")
       .getOrCreate()
 
-    val df = spark.read.json("W:\\HTWG_Master\\Seminar\\data\\cities.json")
-    df.show()
+//    genericLoad(spark)
+//    jsonExample(spark)
+    jsonSQLExample(spark)
 
-    df.select("City").show()
+  }
 
-    df.createOrReplaceTempView("cities")
-
-    val sqlDF = spark.sql("SELECT City, Country FROM cities")
-    sqlDF.show()
-
-
-    //TODO json Example
-
+  def genericLoad(spark: SparkSession): Unit = {
     //TODO csv Example
+    val peopleDFCsv = spark.read.format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load("W:\\HTWG_Master\\Seminar\\data\\people.csv")
 
-    //TODO SQL Example
+    peopleDFCsv.show()
+  }
 
-    //TODO JSON as SQL Example
+  def jsonExample(spark: SparkSession): Unit = {
+    //TODO json Example
+    val df = spark.read.json("W:\\HTWG_Master\\Seminar\\data\\people.json")
+    df.show()
+  }
 
+  def jdbcExample(spark: SparkSession): Unit = {
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:postgresql:dbserver")
+      .option("dbtable", "schema.tablename")
+      .option("user", "username")
+      .option("password", "password")
+      .load()
+
+    val connectionProperties = new Properties()
+    connectionProperties.put("user", "username")
+    connectionProperties.put("password", "password")
+    val jdbcDF2 = spark.read
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
+  }
+
+  def jsonSQLExample(spark: SparkSession): Unit = {
+    val df = spark.read.json("W:\\HTWG_Master\\Seminar\\data\\people.json")
+    df.createOrReplaceTempView("people")
+    val sqlDF = spark.sql("SELECT name, age FROM people")
+    sqlDF.show()
   }
 }
---
+++ src/main/scala/People.scala
@@ -0,0 +1,1 @@
+case class People(name: String, age: Int)
